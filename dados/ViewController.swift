5b4bc6a14899f9d937c7e9312e4862e2fc254b5b
MauricioCardoza <mauricio_cardoza@hotmail.com>
2019-07-10 22:44:14 -0600

    Código de numero al azar

--- dados/ViewController.swift
+++ dados/ViewController.swift
@@ -9,7 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var dado_izquierda: UIImageView!
+    @IBOutlet weak var dado_derecha: UIImageView!
+    var num_derecha : Int = 0
+    var num_izquierda : Int = 0
+    let imagen_dados :[String]
+    
+    let ncaras : UInt32
+    required init?(coder aDecoder: NSCoder) {
+        imagen_dados = ["d1", "d2", "d3", "d4", "d5", "d6"]
+        ncaras = UInt32(imagen_dados.count)
+        super.init(coder: aDecoder)
+    }
+    
+    @IBAction func play_buttom(_ sender: Any) {
+        generar_dados()
+    }
+    
+    func generar_dados(){
+       
+        num_derecha = Int(arc4random_uniform(ncaras))
+        num_izquierda = Int(arc4random_uniform(ncaras))
+        dado_derecha.image = UIImage(named: imagen_dados[num_derecha])
+        dado_izquierda.image = UIImage(named: imagen_dados[num_izquierda])
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
